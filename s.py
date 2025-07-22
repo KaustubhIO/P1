ac0b07030b6a90065e28afdcbf064e7bc124d9ef
Kaustubh07 <kaustubhhadke7@gmail.com>
2025-07-23 02:33:55 +0530

    add new para

---
+++ s.py
@@ -0,0 +1,11 @@
+# name=input("enter your name ")
+# print(f"Good after noon,{name}")
+# #fWrite a program to fill in a letter template given below with name and date.
+# use replace
+# letter = '''Dear <|Name|>, 
+# You are selected! 
+# <|Date|> '''
+# print(letter.replace("<|Name|>","Harry").replace("|<|Date|>","24 september 2025"))
+# use to find anythiny
+name = "ss23ss  wsssss"
+print(name.replace("w","x"))
